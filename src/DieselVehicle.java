b85d44cf592b0e590abef993c4f5fe8169944f1a
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-03 21:19:54 +0600

    Initializing private field and the constructor of DieselVehicle class

--- src/DieselVehicle.java
+++ src/DieselVehicle.java
@@ -1,2 +1,9 @@
 public class DieselVehicle extends Vehicle{
+
+    private int fuelLevel;
+
+    public DieselVehicle(String brand, String model, int year, int fuelLevel) {
+        super(brand, model, year);
+        this.fuelLevel = fuelLevel;
+    }
 }
