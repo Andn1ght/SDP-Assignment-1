b4a074f6f2934eabd7e1acf31b47815c3816f9c8
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-03 21:56:55 +0600

    Initializing private fields and the constructor of GasStation class

--- src/GasStation.java
+++ src/GasStation.java
@@ -1,3 +1,9 @@
 public class GasStation {
+    private RefuelingColumn gasolineColumn;
+    private RefuelingColumn dieselColumn;
 
+    public GasStation(RefuelingColumn gasolineColumn, RefuelingColumn dieselColumn) {
+        this.gasolineColumn = gasolineColumn;
+        this.dieselColumn = dieselColumn;
+    }
 }
