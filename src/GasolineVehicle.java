fbf601bfafee688a6cf260c4b972898b87e4a747
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-03 21:12:30 +0600

    Initializing private field and the constructor of GasolineVehicle class

--- src/GasolineVehicle.java
+++ src/GasolineVehicle.java
@@ -1,2 +1,9 @@
 public class GasolineVehicle extends Vehicle{
+
+    private int fuelLevel;
+
+    public GasolineVehicle(String brand, String model, int year, int fuelLevel) {
+        super(brand, model, year);
+        this.fuelLevel = fuelLevel;
+    }
 }
