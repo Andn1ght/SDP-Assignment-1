119ab5d9df73d9c5d2f983139a19ee1ec6e44301
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-03 22:00:24 +0600

    Adding Singleton pattern to project

--- src/GasStation.java
+++ src/GasStation.java
@@ -1,9 +1,18 @@
 public class GasStation {
+
+    private static GasStation instance;
     private RefuelingColumn gasolineColumn;
     private RefuelingColumn dieselColumn;
 
     public GasStation(RefuelingColumn gasolineColumn, RefuelingColumn dieselColumn) {
         this.gasolineColumn = gasolineColumn;
         this.dieselColumn = dieselColumn;
+    }
+
+    public static GasStation getInstance() {
+        if (instance == null) {
+            instance = new GasStation(new GasolineColumn(), new DieselColumn());
+        }
+        return instance;
     }
 }
